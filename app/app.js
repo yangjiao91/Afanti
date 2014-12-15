0077e63793bb2635edd500bf3c46bd651e4804a1
yangjiao <yangjiao@fun.tv>
2014-12-15 18:27:46 +0800

    init afanti

---
+++ app/app.js
@@ -0,0 +1,113 @@
+'use strict';
+
+angular.module('shipyard', [
+        'ngRoute',
+        'ngCookies',
+        'shipyard.filters',
+        'shipyard.services',
+        'shipyard.controllers',
+        'shipyard.utils',
+        'shipyard.directives',
+        'angular-flash.service',
+        'angular-flash.flash-alert-directive',
+        'angles',
+        'ansiToHtml'
+    ])
+    .config(['$routeProvider', '$httpProvider', '$provide', 'flashProvider',
+        function ($routeProvider, $httpProvider, $provide, flashProvider) {
+            $routeProvider.when('/login', {
+                templateUrl: 'templates/login.html',
+                controller: 'LoginController'
+            });
+            $routeProvider.when('/logout', {
+                template: "",
+                controller: 'LogoutController'
+            });
+            $routeProvider.when('/dashboard', {
+                templateUrl: 'templates/dashboard.html',
+                controller: 'DashboardController'
+            });
+            $routeProvider.when('/applications',{
+                templateUrl: 'templates/applications.html',
+                controller: 'ApplicationsController'
+            });
+
+            $routeProvider.when('/applications/add',{
+                templateUrl: 'templates/application_add.html',
+                controller: 'ApplicationAddController'
+            });
+
+            $routeProvider.when('/containers', {
+                templateUrl: 'templates/containers.html',
+                controller: 'ContainersController'
+            });
+            $routeProvider.when('/containers/deploy', {
+                templateUrl: 'templates/deploy.html',
+                controller: 'DeployController'
+            });
+            $routeProvider.when('/containers/:id', {
+                templateUrl: 'templates/container_details.html',
+                controller: 'ContainerDetailsController'
+            });
+            $routeProvider.when('/containers/:id/logs', {
+                templateUrl: 'templates/container_logs.html',
+                controller: 'ContainerLogsController'
+            });
+            $routeProvider.when('/engines', {
+                templateUrl: 'templates/engines.html',
+                controller: 'EnginesController'
+            });
+            $routeProvider.when('/engines/add', {
+                templateUrl: 'templates/engine_add.html',
+                controller: 'EngineAddController'
+            });
+            $routeProvider.when('/engines/:id', {
+                templateUrl: 'templates/engine_details.html',
+                controller: 'EngineDetailsController'
+            });
+            $routeProvider.when('/events', {
+                templateUrl: 'templates/events.html',
+                controller: 'EventsController'
+            });
+            $routeProvider.otherwise({
+                redirectTo: '/dashboard'
+            });
+            $provide.factory('httpInterceptor', function ($q, $window, flash, AuthToken) {
+                return {
+                    request: function (config) {
+                        return config || $q.when(config);
+                    },
+                    requestError: function (rejection) {
+                        return $q.reject(rejection);
+                    },
+                    response: function (response) {
+                        return response || $q.when(response);
+                    },
+                    responseError: function (rejection) {
+                        switch (rejection.status) {
+                            case 401:
+                                AuthToken.delete();
+                                $window.location.href = '/#/login';
+                                $window.location.reload();
+                                return $q.reject(rejection);
+                                break;
+                            case 403:
+                                flash.error = 'Invalid username/password';
+                                break;
+                        }
+                        return $q.reject(rejection);
+                    }
+                };
+            });
+            $httpProvider.interceptors.push('httpInterceptor');
+            // messages
+            flashProvider.errorClassnames.push('red');
+            flashProvider.warnClassnames.push('yellow');
+            flashProvider.infoClassnames.push('blue');
+            flashProvider.successClassnames.push('green');
+        }]);
+
+
+Chart.defaults.global.responsive = true;
+Chart.defaults.global.animation = false;
+Chart.defaults.global.showTooltips = true;
---
+++ app/services.js
@@ -0,0 +1,49 @@
+'use strict';
+
+angular.module('shipyard.services', ['ngResource', 'ngRoute'])
+    .factory('Login', function($resource) {
+        return $resource('/auth/login', [], {
+            query: { isArray: false },
+            'login': { method: 'POST', isArray: false }
+        });
+    })
+    .factory('Containers', function($resource) {
+        return $resource('/api/containers');
+    })
+    .factory('Container', function($resource) {
+        return $resource('/api/containers/:id/:action', {id: '@id' }, {
+            destroy: { method: 'DELETE' },
+            'save': { isArray: true, method: 'POST' },
+            'control': { isArray: false, method: 'GET' },
+            query: { isArray: false }
+        });
+    })
+    .factory('Engines', function($resource) {
+        return $resource('/api/engines');
+    })
+    .factory('Engine', function($resource) {
+        return $resource('/api/engines/:id', {id: '@id'}, {
+            remove: { method: 'DELETE' },
+            'save': { isArray: true, method: 'POST' },
+            query: { isArray: false }
+        });
+    })
+    .factory('Events', function($resource) {
+        return $resource('/api/events', {}, {
+            'purge': { isArray: false, method: 'DELETE' },
+            query: { isArray: true }
+        });
+    })
+    .factory('ClusterInfo', function($resource) {
+        return $resource('/api/cluster/info', [], {
+            query: { isArray: false }
+        });
+    })
+    .factory('Application', function($resource){
+        return $resource('/api/applications/:id/:action',{id: '@id'},
+            { 
+                'save': {isArray:true,method:POST},
+                'destroy':{method: 'DELETE'},
+                query: {isArray:true}
+       
+    })
